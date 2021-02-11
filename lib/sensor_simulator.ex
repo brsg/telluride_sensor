1f4e68c53c6e853f98ac304a008da8cdebf9371b
Dave Muirhead <dave@brsg.io>
2021-02-11 08:20:58 -0500

    remove generated code

--- lib/sensor_simulator.ex
+++ lib/sensor_simulator.ex
@@ -1,18 +1,2 @@
 defmodule SensorSimulator do
-  @moduledoc """
-  Documentation for `SensorSimulator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SensorSimulator.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
